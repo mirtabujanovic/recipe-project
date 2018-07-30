78798b16168e4b93b5161feb118e2e7bcb8894c7
Mirta Bujanović <mirta.bujanovic@in2.eu>
2018-07-30 12:07:00 +0200

    Defining JPA relationships

---
+++ src/main/java/guru/springframework/recipeproject/domain/Note.java
@@ -0,0 +1,25 @@
+package guru.springframework.recipeproject.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Lob
+    private String recipeNotes;
+    @OneToOne//nećemo definirati cascade jer je Recipe objekt owner veze(ako obrišemo recept neka se brišu i notes, a ne obrnuto)
+    private Recipe recipe;
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public String getRecipeNotes() { return recipeNotes; }
+    public void setRecipeNotes(String recipeNotes) { this.recipeNotes = recipeNotes; }
+
+    public Recipe getRecipe() { return recipe; }
+    public void setRecipe(Recipe recipe) { this.recipe = recipe; }
+}
